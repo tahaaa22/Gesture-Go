c4dac79c4d0bf63dfaf20293fc703230a0581d37
Menna04 <153016363+Menna04@users.noreply.github.com>
2024-04-23 01:12:51 +0200

    DONE

--- Graph.py
+++ Graph.py
@@ -12,6 +12,7 @@ class Graph:
         self.Graph_Window = graph_window
         self.Other_Graph = other_graph  # Reference to the other graph
         self.Paused = False
+        self.CurrentSignal
 
 
     def Browse_Signals(self):
@@ -21,40 +22,22 @@ class Graph:
             Record = wfdb.rdrecord(File_Path[:-4])
             Y_Coordinates = list(Record.p_signal[:, 0])
             X_Coordinates = list(np.arange(len(Y_Coordinates)))
-            Sample_Signal = Signals.Signal(col = "red", X_List=X_Coordinates, Y_list=Y_Coordinates,graphWdg=self.Graph_Window, graphObj=self)
+            self.CurrentSignal = Signals.Signal(col = "red", X_List=X_Coordinates, Y_list=Y_Coordinates,graphWdg=self.Graph_Window, graphObj=self)
 
 
     def Toggle_Play_Pause(self):
         _translate = QtCore.QCoreApplication.translate
+
         self.Paused = not self.Paused
-        if self.Linked:
-            self.Other_Graph.Paused = self.Paused
-            for channel in self.Other_Graph.CHANNELS:
-                if channel.Signal:
-                    channel.Signal.pause = not channel.Signal.pause
-
-            if not self.Paused:
-                self.UI_Window.Play1_Button.setText(_translate("MainWindow", "   Pause         "))
-                self.UI_Window.Play2_Button.setText(_translate("MainWindow", "   Pause         "))
-            else:
-                self.UI_Window.Play1_Button.setText(_translate("MainWindow", "   Play         "))
-                self.UI_Window.Play2_Button.setText(_translate("MainWindow", "   Play         "))
+           
+        if not self.Paused:
+                self.UI_Window.PauseButton.setText(_translate("MainWindow", "   Pause         "))
         else:
-            if self.graph_number == 1:
-                if not self.Paused:
-                    self.UI_Window.Play1_Button.setText(_translate("MainWindow", "   Pause         "))
-                else:
-                    self.UI_Window.Play1_Button.setText(_translate("MainWindow", "   Play         "))
-            else:
-                if not self.Paused:
-                    self.UI_Window.Play2_Button.setText(_translate("MainWindow", "   Pause         "))
-                else:
-                    self.UI_Window.Play2_Button.setText(_translate("MainWindow", "   Play         "))
+                self.UI_Window.PauseButton.setText(_translate("MainWindow", "   Play         "))
 
-        for channel in self.CHANNELS:
-            if channel.Signal:
-                channel.Signal.pause = not channel.Signal.pause
-                self.Graph_Window.getViewBox().setXRange(
-                    max(channel.Signal.X_Coordinates[0: channel.Signal.X_Points_Plotted + 1]) - 100,
-                    max(channel.Signal.X_Coordinates[0: channel.Signal.X_Points_Plotted + 1]))
+        if self.CurrentSignal:
+              self.CurrentSignal.pause = not self.CurrentSignal.pause
+              self.Graph_Window.getViewBox().setXRange(
+                    max(self.CurrentSignal.X_Coordinates[0: self.CurrentSignal.X_Points_Plotted + 1]) - 100,
+                    max(self.CurrentSignal.X_Coordinates[0: self.CurrentSignal.X_Points_Plotted + 1]))
 
